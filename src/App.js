564786aa5f0193f631d741b5dfffff9b00f2a3ad
DragicevicS <steffan95@live.com>
2023-06-22 01:24:42 +0200

    Add basic layout and images for the cards

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <header>
+        <h1>Memory card game</h1>
+        <div className='score-div'>
+            <h3>Current score:</h3>
+            <h3>0</h3>
+            <h3>Best score:</h3>
+            <h3>0</h3>
+        </div>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        <p>Copyright © 2023 DragicevicS</p>
+      </footer>
+    </>
   );
 };
 
