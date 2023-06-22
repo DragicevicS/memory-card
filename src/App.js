ff779f5bf716bf197fb3bf6053f97ddf54786b89
DragicevicS <steffan95@live.com>
2023-06-22 16:59:12 +0200

    Add random shuffling and keeping score

--- src/App.js
+++ src/App.js
@@ -1,21 +1,135 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cards from './components/Cards';
+import biomechBasilisk from './images/biomech-basilisk.png';
+import celestialRavager from './images/celestial-ravager.png';
+import chromatech from './images/chromatech.png';
+import chronospike from './images/chronospike.png';
+import galaxianLeviathan from './images/galaxian-leviathan.png';
+import ionclawAlpha from './images/ionclaw-alpha.png';
+import metamorphisaur from './images/metamorphisaur.png';
+import plasmaDrifter from './images/plasma-drifter.png';
+import synthrosaur from './images/synthrosaur.png';
+import technovore from './images/technovore.png';
+import voidshifter from './images/voidshifter.png';
+import xenothrasher from './images/xenothrasher.png';
 
 function App() {
+  const [ currentScore, setCurrentScore ] = useState(0);
+  const [ bestScore, setBestScore ] = useState(0);
+  const [cards, setCards] = useState([]);
+  const [clickedCards, setClickedCards] = useState([]);
+
+  useEffect(() => {
+    const initialCards = [
+      {
+        id: 0,
+        name: 'Biomech Basilisk',
+        url: biomechBasilisk,
+      },
+      {
+        id: 1,
+        name: 'Celestial Ravager',
+        url: celestialRavager,
+      },
+      {
+        id: 2,
+        name: 'Chromatech',
+        url: chromatech,
+      },
+      {
+        id: 3,
+        name: 'Chronospike',
+        url: chronospike,
+      },
+      {
+        id: 4,
+        name: 'Galaxian Leviathan',
+        url: galaxianLeviathan,
+      },
+      {
+        id: 5,
+        name: 'Ionclaw Alpha',
+        url: ionclawAlpha,
+      },
+      {
+        id: 6,
+        name: 'Metamorphisaur',
+        url: metamorphisaur,
+      },
+      {
+        id: 7,
+        name: 'Plasma Drifter',
+        url: plasmaDrifter,
+      },
+      {
+        id: 8,
+        name: 'Synthrosaur',
+        url: synthrosaur,
+      },
+      {
+        id: 9,
+        name: 'Technovore',
+        url: technovore,
+      },
+      {
+        id: 10,
+        name: 'Voidshifter',
+        url: voidshifter,
+      },
+      {
+        id: 11,
+        name: 'Xenothrasher',
+        url: xenothrasher,
+      },
+    ];
+
+    const shuffledCards = shuffleArray(initialCards);
+    setCards(shuffledCards);
+  }, []);
+
+  const handleCardClick = (clickedCardId) => {
+    if (clickedCards.includes(clickedCardId)) {
+      setCurrentScore(0);
+      setClickedCards([]);
+    } else {
+      setCurrentScore(currentScore + 1);
+      if (currentScore + 1 >= bestScore) {
+        setBestScore(currentScore + 1);
+      }
+  
+      setClickedCards((prevClickedCards) => [...prevClickedCards, clickedCardId]);
+  
+      const shuffledCards = shuffleArray(cards);
+      setCards(shuffledCards);
+    }
+  };
+
+  const shuffleArray = (array) => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  };
+
   return (
     <>
       <header>
         <h1>Memory card game: Sci-Fi Beasts Edition</h1>
         <div className='score-div'>
             <h3>Current score:</h3>
-            <h3>0</h3>
+            <h3>{currentScore}</h3>
             <h3>Best score:</h3>
-            <h3>0</h3>
+            <h3>{bestScore}</h3>
         </div>
       </header>
       <main>
         <h2>Get points by clicking on an image but don't click on any more than once!</h2>
-        <Cards />
+        <Cards cards={cards} onCardClick={handleCardClick} />
       </main>
       <footer>
         <p>Copyright © 2023 DragicevicS</p>
@@ -23,6 +137,5 @@ function App() {
     </>
   );
 };
-
 
 export default App;
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,101 +1,12 @@
 import React from 'react';
-import biomechBasilisk from '../images/biomech-basilisk.png';
-import celestialRavager from '../images/celestial-ravager.png';
-import chromatech from '../images/chromatech.png';
-import chronospike from '../images/chronospike.png';
-import galaxianLeviathan from '../images/galaxian-leviathan.png';
-import ionclawAlpha from '../images/ionclaw-alpha.png';
-import metamorphisaur from '../images/metamorphisaur.png';
-import plasmaDrifter from '../images/plasma-drifter.png';
-import synthrosaur from '../images/synthrosaur.png';
-import technovore from '../images/technovore.png';
-import voidshifter from '../images/voidshifter.png';
-import xenothrasher from '../images/xenothrasher.png';
 
-function Cards() {
-  const cards =
-    [
-      {
-        id: 1,
-        name: 'Biomech Basilisk',
-        url: biomechBasilisk,
-        clicked: false
-      },
-      {
-        id: 2,
-        name: 'Celestial Ravager',
-        url: celestialRavager,
-        clicked: false
-      },
-      {
-        id: 3,
-        name: 'Chromatech',
-        url: chromatech,
-        clicked: false
-      },
-      {
-        id: 4,
-        name: 'Chronospike',
-        url: chronospike,
-        clicked: false
-      },
-      {
-        id: 5,
-        name: 'Galaxian Leviathan',
-        url: galaxianLeviathan,
-        clicked: false
-      },
-      {
-        id: 6,
-        name: 'Ionclaw Alpha',
-        url: ionclawAlpha,
-        clicked: false
-      },
-      {
-        id: 7,
-        name: 'Metamorphisaur',
-        url: metamorphisaur,
-        clicked: false
-      },
-      {
-        id: 8,
-        name: 'Plasma Drifter',
-        url: plasmaDrifter,
-        clicked: false
-      },
-      {
-        id: 9,
-        name: 'Synthrosaur',
-        url: synthrosaur,
-        clicked: false
-      },
-      {
-        id: 10,
-        name: 'Technovore',
-        url: technovore,
-        clicked: false
-      },
-      {
-        id: 11,
-        name: 'Voidshifter',
-        url: voidshifter,
-        clicked: false
-      },
-      {
-        id: 12,
-        name: 'Xenothrasher',
-        url: xenothrasher,
-        clicked: false
-      },
-    ];
-
-  
+function Cards({ cards, onCardClick}) { 
     return (
       <div className='cards-container'>
         {cards.map((card) => (
-          <div key={card.id} className='card'>
+          <div key={card.id} className='card' onClick={() => onCardClick(card.id)}>
             <div>
-              <img src={card.url} alt='Card' />
+              <img src={card.url} alt='Card' draggable='false' />
             </div>
             <div>
               <h3>{card.name}</h3>
